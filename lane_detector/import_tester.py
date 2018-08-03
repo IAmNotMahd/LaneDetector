10ccd312822084da5562da1c837eb12a5699f222
IAmNotMahd <mahdlegend@gmail.com>
2018-08-03 16:15:27 -0400

    Make executable command (using Chase's cheat)

--- lane_detector/import_tester.py
+++ lane_detector/import_tester.py
@@ -3,6 +3,7 @@
 import argparse
 from lane_detector import frame_list
 # import frame_list
+
 parser = argparse.ArgumentParser()
 parser.add_argument("source", help="Path to video or image directory")
 parser.add_argument("-e", "--environ", help="USE Environment Variables instead", action="store_true", default=False)
@@ -10,7 +11,7 @@ parser.add_argument("-s", "--scnn", help="RUN SCNN probability map generation", 
 parser.add_argument("-v", "--video", help="RUN video generation", action="store_true", default=False)
 parser.add_argument("-d", "--debug", help="RUN in debug mode (output displayed)", action="store_true", default=False)
 parser.add_argument("-c", "--clean", help="REMOVE all generated folders and files", action="store_true", default=False)
-args = vars(parser.parse_args())
+kwargs = vars(parser.parse_args())
 
-scnn_test = frame_list.SCNN(**args)
+scnn_test = frame_list.SCNN(**kwargs)
 print(scnn_test.run_all())
