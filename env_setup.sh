b8f454204b24b5b15d1ea35af613e4418816bf1f
IAmNotMahd <mahdlegend@gmail.com>
2018-08-06 11:06:24 -0400

    installs cuda now as well

--- env_setup.sh
+++ env_setup.sh
@@ -45,3 +45,29 @@ sudo ldconfig
 
 # 4. EXECUTE SOME OPENCV EXAMPLES AND COMPILE A DEMONSTRATION
 # To complete this step, please visit 'http://milq.github.io/install-opencv-ubuntu-debian'.
+
+
+# ***************
+# INSTALL CUDA Toolkit
+# ***************
+
+# 1. Get the package from Nvidia
+# http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
+
+# 2. Set environment variables
+echo 'export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
+' >> ~/.bashrc
+
+# 3. Let terminal know of the changes to the .bashrc file
+source .bashrc
+sudo apt-get update 
+sudo apt-get install -y cuda
+
+# 4. Check if installation is successful by running the next line
+# nvcc -V
